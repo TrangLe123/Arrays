dcc8e8b7193958686d6749a6ff30eeff37bfa102
Trang Le <tle20@usf.edu>
2021-02-05 00:56:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int [25];
+            for (int i = 0; i < 26; i++)
+            {
+                number[i] = i + 1;
+                Console.WriteLine("Element value = " + number[i]);
+            }
+        }
+    }
+}
